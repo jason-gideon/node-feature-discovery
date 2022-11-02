0d2e3c638bd4060b5920457c78e929484f08b7ea
unknown <siyuan.cao@iluvatar.local>
2022-11-02 18:09:44 +0800

    fix init include bug

--- pkg/nfd-client/worker/nfd-worker.go
+++ pkg/nfd-client/worker/nfd-worker.go
@@ -49,6 +49,7 @@ import (
 	_ "sigs.k8s.io/node-feature-discovery/source/storage"
 	_ "sigs.k8s.io/node-feature-discovery/source/system"
 	_ "sigs.k8s.io/node-feature-discovery/source/usb"
+	_ "sigs.k8s.io/node-feature-discovery/source/gpu"
 )
 
 // NFDConfig contains the configuration settings of NfdWorker.
